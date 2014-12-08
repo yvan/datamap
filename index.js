f88781ebb7d22e72a22190cd24f9527b7c5a71c1
Ivan <yvanscher@gmail.com>
2014-12-08 00:28:29 -0500

    works

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var handlebars = require('handlebars')
-                 .create({defaultLayout:'main'})
+var handlebars = require('express-handlebars')
 var express = require('express')
 var three = require('three')
 var app = express()
 
-app.engine('handlebars', handlebars.engine)
-app.set('view engine', handlebars)
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', function(req, res){
-  res.render('index',greeting)
+app.get('/', function(req,res){
+  res.send('your mom')
+  res.end()
+})
+
+app.get('/home', function(req, res){
+  res.render('home',{'item':'pizza', 'description':'tasty'})
 })
 
 var template = 'Node app is running at localhost: {port~number}'
