925feb1124203de598f09f0872856b33f5a9e3e7
Ivan <yvanscher@gmail.com>
2014-12-07 22:56:27 -0500

    fixed bug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname+'/public'))
 
 app.get('/', function(req, res){
-  response.send('blah')
+  res.send('blah')
 })
 
 var template = 'Node app is running at localhost: {port~number}'
