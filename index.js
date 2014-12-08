9bf3acd83709eb9abf157f56c15da70376f5e313
Ivan <yvanscher@gmail.com>
2014-12-07 23:23:33 -0500

    rendering handlebars

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+var handlebars = require('express3-handlebars')
+                 .create({defaultLayout:'main'})
 var express = require('express')
 var three = require('three')
 var app = express()
 
+app.engine('handlebars', handlebars.engine)
+app.set('view engine', handlebars)
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname+'/public'))
+//app.use(express.static(__dirname+'/public'))
 
 app.get('/', function(req, res){
-  res.send('blah')
+  res.render('index',greeting)
 })
 
 var template = 'Node app is running at localhost: {port~number}'
