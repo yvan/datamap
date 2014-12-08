272e892687b84a78ae3e2cfe18d114985d40de96
Ivan <yvanscher@gmail.com>
2014-12-07 22:55:39 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var three = require('three')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname+'/public'))
+
+app.get('/', function(req, res){
+  response.send('blah')
+})
+
+var template = 'Node app is running at localhost: {port~number}'
+var txt = template.replace('{port~number}', app.get('port'))
+
+app.listen(app.get('port'), function(){
+  console.log(txt)
+})
