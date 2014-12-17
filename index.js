be9593d583e2aca7a3bb77649b8ee739de483b7a
Ivan <yvanscher@gmail.com>
2014-12-17 15:37:32 -0500

    got rid of that crappy code

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 var handlebars = require('express-handlebars')
 var express = require('express')
-//var three = require('three')
-//var dThree = require('d3')
 var path = require('path')
 var app = express()
 
-//app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-//app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/static'));
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(req, res){
-  //res.render('home')
   res.sendFile(__dirname +'/index.html')
 })
 
